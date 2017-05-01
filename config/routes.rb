135bdafc810a90da47481f6e316771d2fbd8f39f
taylordorin <taylordorin@gmail.com>
2017-05-01 17:32:52 -0400

    created react router

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
 
+  get '/recipes/*path', to: 'static_pages#index'
+
   namespace :api do
     namespace :v1 do
       resources :recipes, only: [:index, :show, :create]
