47cfe4260d70a3401e1ef0e85f162c8e66cf8ff2
taylordorin <taylordorin@gmail.com>
2017-05-04 13:04:24 -0400

    added the ingredients and instructions to be passed to the recipe show page.

--- react/src/components/InstructionTile.js
+++ react/src/components/InstructionTile.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const IngredientTile = (props) => {
+const InstructionTile = (props) => {
   return(
     <div className="row">
         <div className="recipe-container">
           <div className="ingredient-show ">
-             <div className="recipe-text">{props.quantity} {props.unit} {props.ingredient_name} </div>
+             <div className="recipe-text">{props.step} {props.direction} </div>
           </div>
         </div>
     </div>
   )
 }
 
-export default IngredientTile;
+export default InstructionTile;
