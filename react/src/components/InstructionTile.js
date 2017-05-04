536f523f2a40702f2db22f0b72d6788eb98c135b
taylordorin <taylordorin@gmail.com>
2017-05-04 18:08:20 -0400

    Added ability to display one direction at a time

--- react/src/components/InstructionTile.js
+++ react/src/components/InstructionTile.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import RecipeShowContainer from '../containers/RecipeShowContainer'
 
-const InstructionTile = (props) => {
-  return(
-    <div className="row">
-        <div className="recipe-container">
-          <div className="ingredient-show ">
-             <div className="recipe-text">{props.step} {props.direction} </div>
+class InstructionTile extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    let hidden;
+    if (true === this.props.hidden) {
+      hidden = 'hidden';
+    } else {
+      hidden = '';
+    }
+
+    let plusMinusIcon;
+    let openbutton;
+    if (this.props.hidden) {
+      plusMinusIcon = `${this.props.step}`;
+      openbutton = "openbutton plus";
+    } else {
+      plusMinusIcon = '-';
+      openbutton = "openbutton minus";
+    }
+
+    return(
+      <div className="row">
+        <div className="small-1 columns">
+          <div className={openbutton}
+            onClick={this.props.handleClick}>{plusMinusIcon}
           </div>
         </div>
-    </div>
-  )
+        <div className="small-11 columns">
+          <p className={hidden}>{this.props.direction}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default InstructionTile;
