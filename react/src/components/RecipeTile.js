5707f9583d3f63a0e00db2b950087ad17d5c674d
taylordorin <taylordorin@gmail.com>
2017-05-03 21:19:31 -0400

    updated some styling and allowing the RecipeTile page to see all contents of Recipe array

--- react/src/components/RecipeTile.js
+++ react/src/components/RecipeTile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { browserHistory, Link } from 'react-router';
 
-const RecipeTile = (props) => {
+const RecipeTile = ({ id, recipe_name, category, cook_time, skill_level }) => {
   const divStyle = {
       borderRight: '1pt solid grey',
       width: '129px'
@@ -13,19 +13,19 @@ const RecipeTile = (props) => {
     <div className="recipe-container">
       <div className="recipe-content">
          <h1 className="heading-1">
-           {props.recipe_name}
+           <Link to={`/recipes/${id}`}>{recipe_name}</Link>
          </h1>
          <ul className="recipe-list meta">
            <li className="recipe-item" style={divStyle}>
-             <div className="recipe-value">{props.category}</div>
+             <div className="recipe-value">{category}</div>
              <div className="recipe-text">Category</div>
            </li>
            <li className="recipe-item" style={divStyle2}>
-             <div className="recipe-value">{props.cook_time}</div>
+             <div className="recipe-value">{cook_time}</div>
              <div className="recipe-text">Mins</div>
            </li>
            <li className="recipe-item">
-             <div className="recipe-value">{props.skill_level}</div>
+             <div className="recipe-value">{skill_level}</div>
              <div className="recipe-text">Skill Level</div>
            </li>
          </ul>
